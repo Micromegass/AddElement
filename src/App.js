a5a4e3fa0f3933896007dd55fed5fc9d8ae02d65
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-10-10 16:35:20 -0500

    add Element

--- src/App.js
+++ src/App.js
@@ -2,18 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+
+      tasks: ["tarea1", "tarea2"]
+    }
+  }
+
+
   render() {
     return (
-    
+      <div>
+        <h1> Hola Mundo </h1>
+        <ul>
 
-   <div>
-        <h1> Hola Axel</h1> 
-        <p> wassup</p>
+          {this.state.tasks.map(task =>
 
-   </div> 
+            <li>{task}</li>
 
+          )}
+
+        </ul>
+
+
+            <button onClick={this.addTask.bind(this)}> Add Task </button>
+
+      </div>
     );
   }
+
+
+  addTask() {
+
+      this.setState({
+
+        tasks: this.state.tasks.concat('New Element')
+
+      });
+
+
+  }
+
 }
 
 export default App;
